faa6ffb1afeea037b2c122a89ca8c468d5ef577a
Jungwook Lee <jungwook.l.92@gmail.com>
2020-01-30 16:57:09 -0500

    Add more files

---
+++ tree/checkIdenticalTree.py
@@ -0,0 +1,14 @@
+from tree import TreeNode, Tree
+
+""" Check if two binary trees are identical or not.
+    Assumptions:
+        - two separate instances of binary trees
+"""
+
+# TODO:recursive
+def checkIdenticalTree(t1, t2)
+    pass
+
+# TODO:iterative 
+def checkIdenticalTree(t1, t2)
+    pass
