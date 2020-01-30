5301feed9a9c6bab98d7d8ddb901dea38666771a
Jungwook Lee <jungwook.l.92@gmail.com>
2020-01-30 17:51:24 -0500

    Implement inorder with stack

--- tree/checkIdenticalTree.py
+++ tree/checkIdenticalTree.py
@@ -4,8 +4,7 @@ from tree import TreeNode, Tree
     Assumptions:
         - two separate instances of binary trees
 """
-
-# TODO:recursive
+# Runtime O(nodes), Space O(nodes)
 def checkIdenticalTree(t1, t2):
     if not t1 and not t2:
         return True
@@ -17,4 +16,31 @@ def checkIdenticalTree(t1, t2):
     else:
         return False
 
-# TODO:iterative 
+# TODO:iterative, no stack ("Morris Traversal")
+# def print_preorder(t1, t2):
+#     # how to traverse iteratively?
+#     while t1:
+#         while t1:
+#             t1 = t1.left
+#         # print left value
+#         print(t1.val)
+#         if t1.right:
+#             t1 = t1.right
+
+# TODO: iterative with extra memory (stack)
+def print_inorder(t1):
+    cur, stack = t1, []
+    while True:
+        # while the left node exists keep pushing
+        while cur:
+            stack.append(cur)
+            cur = cur.left
+        if not stack:
+            break
+        # Once no more left, pop one out
+        node = stack.pop()
+        # Print value
+        print(node.val)
+        # push the right node value if one exists
+        cur = node.right
+
