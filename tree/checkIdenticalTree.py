c49a686b382949fcf072ff712a3565830976d8d4
Jungwook Lee <jungwook.l.92@gmail.com>
2020-02-04 16:20:12 -0500

    Implement checkidentical tree iteratively

--- tree/checkIdenticalTree.py
+++ tree/checkIdenticalTree.py
@@ -15,3 +15,27 @@ def checkIdenticalTree(t1, t2):
                checkIdenticalTree(t1.right, t2.right)
     else:
         return False
+
+# Runtime O(n) and O(n)
+def checkIdenticalTree_iter(t1, t2):
+    cur1, cur2, stack1, stack2 = t1, t2, [], []
+    while True:
+        while cur1 and cur2:
+            stack1.append(cur1)
+            stack2.append(cur2)
+            cur1 = cur1.left
+            cur2 = cur2.left
+            # note that ^ operator don't support []
+            if (not cur1)^(not cur2):
+                return False
+        if not stack1 and not stack2:
+            return True
+        elif (not stack1) ^ (not stack2):
+            return False
+        else:
+            node1 = stack1.pop()
+            node2 = stack2.pop()
+        if node1.val != node2.val:
+            return False
+        cur1 = node1.right
+        cur2 = node2.right
--- tree/test_checkIdenticalTree.py
+++ tree/test_checkIdenticalTree.py
@@ -1,7 +1,7 @@
 import unittest
 
 from tree import TreeNode, build_tree
-from checkIdenticalTree import checkIdenticalTree
+from checkIdenticalTree import checkIdenticalTree, checkIdenticalTree_iter
 
 class CheckIdenticalTreeTestCase(unittest.TestCase):
     def test_identical_tree(self):
@@ -9,17 +9,36 @@ class CheckIdenticalTreeTestCase(unittest.TestCase):
         t1 = build_tree(test_in)
         t2 = build_tree(test_in)
         self.assertTrue(checkIdenticalTree(t1, t2))
+        self.assertTrue(checkIdenticalTree_iter(t1, t2))
 
     def test_not_same_tree(self):
         test_in = [1,2,3,4,5]
         t1 = build_tree(test_in)
         t2 = build_tree([1])
         self.assertFalse(checkIdenticalTree(t1, t2))
+        self.assertFalse(checkIdenticalTree_iter(t1, t2))
+
+        # Flip input trees
+        test_in = [1,2,3,4,5]
+        t2 = build_tree(test_in)
+        t1 = build_tree([1])
+        self.assertFalse(checkIdenticalTree(t1, t2))
+        self.assertFalse(checkIdenticalTree_iter(t1, t2))
 
     def test_empty_trees(self):
         t1 = build_tree([])
         t2 = build_tree([])
         self.assertTrue(checkIdenticalTree(t1, t2))
+        self.assertTrue(checkIdenticalTree_iter(t1, t2))
+
+    def test_check_iter(self):
+        t1 = build_tree([1,2,3,4])
+        t2 = build_tree([1,2,3,4])
+        self.assertTrue(checkIdenticalTree(t1, t2))
+
+        t1 = build_tree([1,2,3,1])
+        t2 = build_tree([1,2,3,4])
+        self.assertFalse(checkIdenticalTree(t1, t2))
 
 if __name__ == '__main__':
     unittest.main()
